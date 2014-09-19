8a49637b0c027624d9a80d5e82e9a628f3215252
haksup <ohhs@cspi.co.kr>
2014-09-19 17:39:05 +0900

    ok

---
+++ src/main/webapp/angularTest/app.js
@@ -0,0 +1,15 @@
+var todoList = [
+	{done : true, title : "AngularJS 독서"},
+	{done : false, title:"AngularJS 공부하기"},
+	{done : false, title:"개인 프로젝트 구성"}
+];
+
+function todoCtrl($scope){
+	$scope.appName='AngularJS TODO APP';
+	$scope.todoList = todoList;
+	
+	$scope.addNewTodo = function(newTitle){
+		todoList.push({done:false, title:"newTitle"});
+		$scope.newTitle='';
+	}
+}
