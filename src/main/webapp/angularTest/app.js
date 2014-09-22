85b0d24a20790737c865d8aa8a365c63b6cb7a8a
haksup <ohhs@cspi.co.kr>
2014-09-22 18:01:23 +0900

    angularJS

--- src/main/webapp/angularTest/app.js
+++ src/main/webapp/angularTest/app.js
@@ -4,12 +4,42 @@ var todoList = [
 	{done : false, title:"개인 프로젝트 구성"}
 ];
 
-function todoCtrl($scope){
+//function todoCtrl($scope){
+//	$scope.appName='AngularJS TODO APP';
+//	$scope.todoList = todoList;
+//	
+//	$scope.addNewTodo = function(newTitle){
+//		todoList.push({done:false, title:"newTitle"});
+//		$scope.newTitle='';
+//	};
+//}
+
+angular.module('myAppName', [])
+	.controller('todoCtrl', function($scope) {
+		
 	$scope.appName='AngularJS TODO APP';
 	$scope.todoList = todoList;
 	
 	$scope.addNewTodo = function(newTitle){
 		todoList.push({done:false, title:"newTitle"});
 		$scope.newTitle='';
-	}
-}
+	};
+	
+	$scope.archive = function(){
+		for(var i=$scope.todoList.length - 1; i >= 0; i--){
+			if($scope.todoList[i].done){
+				$scope.todoList.splice(i,1);
+			}
+		}
+	};
+	
+	$scope.remain = function(){
+		var remainCount = 0;
+		angular.forEach($scope.todoList, function(value, key){
+			if(value.done === false){
+				remainCount++;
+			}
+		});
+		return remainCount;
+	};
+});
